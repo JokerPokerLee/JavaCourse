b2f137ae1a4ec8821c493626e6b2bbcce17dd626
Joker Poker <lihaoran@mail.ustc.edu.cn>
2016-01-21 00:33:47 +0800

    cannot paint the board

--- FlipGame/FlipGame.java
+++ FlipGame/FlipGame.java
@@ -6,14 +6,14 @@ import javax.swing.*;
 
 class MyWindow extends JFrame implements ActionListener {
 
-	final int WIDE = 800, HIGH = 800;		//full window size
+	int WIDE = 800, HIGH = 800;		//full window size
 	final int MARGIN = 20;				//shortest length of margin
 	final int LENGTH = 10;				//shortest length of grid
 	final int ox = 10, oy = 10;
-	final int len = 10;
+	final int len = 100;
 
 	Container con = getContentPane();
-	JButton grid[];
+	JButton[] grid;
 
 	JTextField rowCon, columnCon;
 	JLabel rowLab, columnLab;
@@ -25,28 +25,43 @@ class MyWindow extends JFrame implements ActionListener {
 	MyWindow(String s) {
 
 		super(s);
+
+        int len = 50, tal = 20, sep = 5, bias = 21;
+        int L = (WIDE - len * 8 - sep * 4) / 2;
+
 		rowLab = new JLabel("Row");
+        rowLab.setBounds(L + bias, sep, len, tal);
+
 		rowCon = new JTextField(10);
-		columnLab = new JLabel("Column");
+        rowCon.setBounds(L + len + sep, sep, len * 2, tal);
+
+		columnLab = new JLabel("Col");
+        columnLab.setBounds(L + len * 3 + sep * 2 + bias + 4, sep, len, tal);
+
 		columnCon = new JTextField(10);
+        columnCon.setBounds(L + len * 4 + sep * 3, sep, len * 2, tal);
+
 		confirm = new JButton("Confirm");
+        confirm.setBounds(L + len * 6 + sep * 4, sep, len * 2, tal);
 
 		confirm.addActionListener(this);
 
-		con.setLayout(new FlowLayout());
+
+		con.setLayout(null);
+		con.setBackground(Color.pink);
 		con.add(rowLab);
 		con.add(rowCon);
 		con.add(columnLab);
 		con.add(columnCon);
 		con.add(confirm);
-		con.setBounds(100, 100, 200, 150);
-		con.setBackground(Color.pink);
-	    con.setPreferredSize(new Dimension(900,600));
-        pack();
-        setResizable(true);
+
+        setSize(WIDE, HIGH);
+        setResizable(false);
 		setVisible(true);
 		validate();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        WIDE -= tal + sep * 2;
 	}
 
 	void Paint() {
@@ -61,37 +76,51 @@ class MyWindow extends JFrame implements ActionListener {
 		ox = (WIDE - m * len) / 2;
 		oy = (HIGH - n * len) / 2;
 
+        /*
+        System.out.println(ox + " " + oy + " " + len);
+        JButton tmp = new JButton("test");
+        tmp.setBounds(ox, oy, 100, 100);
+        con.add(tmp);
+        validate();
+        if (1 == 1)
+            return;
+        */
+
 		int tot = n * m;
-		grid = new JButton[tot];
-		for (int i = 0; i < tot; i++) {
+		for (int i = 0; i < 1; i++) {
 			int x = i / m;
 			int y = i % m;
-			con.add(grid[i]);
-			grid[i].setBound();
+            grid[i].addActionListener(this);
+			//grid[i].setBounds(ox + len * x, oy + len * y, len, len);
+            //grid[i].addActionListener(this);
+			//con.add(grid[i]);
 		}
+        
+        setVisible(true);
+		validate();
 
-        for (int i = 0; i < tot; i++) {
-            sta[i] = Math.random() > 0.5 ? 1 : 0;
-            grid[i].setBackground(sta[i] == 1 ? Color.White : Color.Black);
-        }
+        //for (int i = 0; i < tot; i++) {
+        //    sta[i] = Math.random() > 0.5 ? 1 : 0;
+        //    grid[i].setBackground(sta[i] == 1 ? Color.white : Color.black);
+        //}
 	}
 
     private void flip(int k) {
         sta[k] ^= 1;
-        grid[k].setBackground(sta[k] == 1 ? Color.White : Color.Black);
+        grid[k].setBackground(sta[k] == 1 ? Color.white : Color.black);
     }
 
     private boolean check() {
-        int cnt = 0;
+        int cnt = 0, tot = n * m;
         for (int i = 0; i < tot; i++)
             cnt += sta[i];
-        return cnt == 0 || cnt == n * m;
+        return cnt == 0 || cnt == tot;
     }
 
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == confirm) {
-            n = Interger.parseInt(row.getText());
-            m = Interger.parseInt(column.getText());
+            n = Integer.parseInt(rowCon.getText());
+            m = Integer.parseInt(columnCon.getText());
 			Paint();
             return;
 		}
@@ -109,9 +138,8 @@ class MyWindow extends JFrame implements ActionListener {
             flip(k - m);
         if (x < n - 1)
             flip(k + m);
-        if (check()) {
-            JOptionPane.showMessageDialog(frame, "You Win!");
-        }
+        if (check())
+            JOptionPane.showMessageDialog(this, "You Win!");
 	}
 }
 
